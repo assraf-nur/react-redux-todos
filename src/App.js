0f1b205ab5e9ab7c0372fc0b18cf5724a3a95c4f
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-07-17 23:28:23 +0600

    add useeffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Todos from './Components/Todos';
 
 function App() {
   return (
     <div>
-      <h1 className='text-center font-semibold text-5xl mt-5'>ToDo S</h1>
+      <Todos></Todos>
     </div>
   );
 }
--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { getAllTodos } from '../Services/Actions/Action';
 
 const Todos = () => {
+
+    const {loading, todos, error} = useSelector(state => state);
+
+    const dispatch = useDispatch();
+    
+    useEffect(() =>{
+        dispatch(getAllTodos());
+    },[])
+
     return (
         <div>
+            <h1 className='text-center font-semibold text-5xl mt-5'>ToDo S</h1>
+
+            {
+            loading ? <h1 className='text-center font-semibold text-5xl mt-5'>Loading...</h1> : null
+            }
+            {
+            error ? <h1 className='text-center font-semibold text-5xl mt-5'>Error...</h1> : null
+            }
             
         </div>
     );
